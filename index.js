049533970df599178fa22c1d8e890b174f7148dd
Adam Gordon <adamjgordon24@gmail.com>
2019-01-09 02:25:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay (musicians,instruments) {
-  lineUp = [];
+  var lineUp = [];
   for (var i = 0; i <musicians.length; i++) {
     lineUp.push(musicians[i]+"plays " + instruments[i])
   }
